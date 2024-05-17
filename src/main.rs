a5f71016bf4630a88ae4140f255bbef3b3378e4c
DouglasAgostinho <marques.aut@gmail.com>
2024-05-16 21:00:45 -0300

    Adding function get num()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+
+use std::io;
+
+
+fn get_num() -> f32{
+    
+    let mut num: String = String::new();
+
+    match io::stdin().read_line(&mut num) {
+
+        Ok(n) => n,
+        Err(e) => {
+            println!("The value inserted {}, will cause Error: {}. Num will be set to 0.0 (zero)", num, e);
+            return 0.0;
+        }        
+    };
+
+    return match num.trim().parse::<f32>() {
+        Ok(i) => i,
+        Err(e) => {
+            println!("The value inserted {}, caused the Error {}. Num will be set to 0.0 (zero)", num, e);
+            return 0.0;
+        }        
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Testing function get num {}", get_num());
+    
 }
